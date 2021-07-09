fde9eb51f81ae8beecd26f6359b672345328347d
adbailey4 <andbaile@ucsc.edu>
2021-07-09 16:46:51 +0000

    update run_guppy.sh and move publication.md to README.md

--- run_guppy.sh
+++ run_guppy.sh
@@ -8,7 +8,7 @@ do
     for experiment_dir in $(ls "$fast5/$model")
     do
       echo "$fast5/$model/$experiment_dir/fast5"
-      guppy_basecaller --input_path "$fast5/$model/$experiment_dir/fast5" --save_path "$output_dir/$model/" --config rna_r9.4.1_70bps_hac.cfg --device "cuda:0"
+      guppy_basecaller --input_path "$fast5/$model/$experiment_dir/fast5" --save_path "$output_dir/$model/$experiment_dir/" --config rna_r9.4.1_70bps_hac.cfg --device "cuda:0"
     done
   fi
 done
