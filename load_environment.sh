95b14c5963a3e4c57525f1e802f60dedaebc5dc5
Ubuntu <ubuntu@ip-172-31-43-126.us-west-2.compute.internal>
2021-07-09 16:40:41 +0000

    first commit

---
+++ load_environment.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cwd=`pwd`
+export PATH="$PATH:$cwd/src/cmake/bin"
+export PATH="$PATH:$cwd/src/bwa/"
---
+++ run_guppy.sh
@@ -0,0 +1,16 @@
+fast5="/home/ubuntu/data"
+output_dir="/home/ubuntu/fastq"
+for model in $(ls "$fast5")
+do
+  if [[ -d "$fast5/$model" ]]
+  then
+    echo $model
+    for experiment_dir in $(ls "$fast5/$model")
+    do
+      echo "$fast5/$model/$experiment_dir/fast5"
+      guppy_basecaller --input_path "$fast5/$model/$experiment_dir/fast5" --save_path "$output_dir/$model/" --config rna_r9.4.1_70bps_hac.cfg --device "cuda:0"
+    done
+  fi
+done
+
+
